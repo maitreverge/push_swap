ab76a328ce86f4a6622d9cda90dba8059686ae38
de_la_verge <verge.f@gmail.com>
2023-12-27 18:41:19 +0100

    init double function

--- src/arg_parsing.c
+++ src/arg_parsing.c
@@ -6,11 +6,16 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 12:30:50 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/27 16:20:04 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/27 18:41:06 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+int	check_double(t_node *a, int nb)
+{
+	
+}
 
 void	init_stack(t_node **a, char **av)
 {
@@ -24,6 +29,8 @@ void	init_stack(t_node **a, char **av)
 		if (current_number > INT_MAX || current_number < INT_MIN)
 			exit(-1);
 		// ! NUMBER REPETITION
+		if (check_double(*a, int(current_number)))
+			exit(-1);
 		
 		
 		av++;
