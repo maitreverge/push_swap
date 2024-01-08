bf76fc934f27d11704b8448897193c5a4fc9280c
flverge_42 <verge.f@gmail.com>
2024-01-08 13:58:42 +0100

    fast_save

--- src/init_stack.c
+++ src/init_stack.c
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:25:40 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/08 13:40:44 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/08 13:53:13 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,49 @@ void	quit(long *to_free, int exit_code)
 	exit(exit_code);
 }
 
+int		check_double(long *to_check, int j)
+{
+	int	i;
+
+	i = 0;
+	while (i < j)
+	{
+		if (to_check[i])
+		i++;
+	}
+	return (0)
+}
+
 void    init_stack(int ac, char **av, t_node **a)
 {
 	long	cur_nb;
 	long	*pre_buff;
 	int		i;
+	int 	j;
 
-	i = 0;    
+	i = 0;
+	j = 0;
 	if (ac == 2)
 			av = ft_split(av[1], ' ');
 	// ! calculate number of args
 	while (av[i] != NULL)
 		i++;
 	pre_buff = malloc((sizeof(long) * i) + 1);
+	if (!pre_buff)
+		exit(-1);
 	// assign in a tab + check double AND limits
-	while (*++av != NULL)
+	while (*++av != NULL) // *++av => skips the first av
 	{
 		cur_nb = ft_atol(*av);
-		if (cur_nb < INT_MIN || cur_nb > INT_MAX)
+		if ((cur_nb < INT_MIN || cur_nb > INT_MAX))
 			quit(pre_buff, -1);
 		printf("Current nb = %li\n", cur_nb);
-	}    
+		pre_buff[j] = cur_nb;
+		if (check_double(pre_buff))
+			quit(pre_buff, -1);
+		j++;
+	}
+	pre_buff[j] = NULL;
+	 
 	
 }
