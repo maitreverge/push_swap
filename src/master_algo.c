7a5389d760eae58c23abe256e3257b8dd4fe965a
flverge_42 <verge.f@gmail.com>
2024-01-09 14:35:24 +0100

    working on already_sorted function

--- src/master_algo.c
+++ src/master_algo.c
@@ -6,16 +6,37 @@
 /*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:20:28 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/09 14:07:13 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/09 14:33:10 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+int     already_sorted(t_node **a)
+{
+    t_node *current;
+    t_node *next;
+
+    current = *a;
+    next = current->next;
+    while (current->next != NULL)
+    {
+        if (current->nb > next->nb)
+            return (1);
+        current = next;
+        next = current->next;
+    }
+    return (0);
+}
 
 void    master_algo(t_node **a, t_node **b)
 {
     int len_stack;
 
     len_stack = lstsize(*a);
-    printf("Len stack = %i\n", len_stack);
+    // printf("Len stack = %i\n", len_stack);
+    // ! Check if stack is already sorted
+    if (!already_sorted(a))
+        printf("Shit's already sorted\n");
+    
 }
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,18 +6,19 @@
 /*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 11:55:34 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/09 14:06:58 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/09 14:32:52 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	print_stack(t_node *a)
+void	print_stack(t_node *a, char *message)
 {
 	t_node *current;
 
 	current = a;
 
+	printf("%s", message);
 	while (current->next != NULL)
 	{
 		printf("Node = %i\n", current->nb);
@@ -36,10 +37,12 @@ int	main(int ac, char **av)
 	if (!check_arg(ac, av))
 	{
 		init_stack(ac, av, &a);
+		// print_stack(a, "Stack A Pre-Algo\n");
 		master_algo(&a, &b);
-		print_stack(a);
+		// print_stack(a, "Stack A Post-Algo\n");
 		free_stack(a);
-		free_stack(b);
+		if (b != NULL)
+			free_stack(b);
 	}
 	else
 		printf("Error\n");
