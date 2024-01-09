9f976f230cf56d3d397bfd9b1d02a05ce8a19755
flverge_42 <verge.f@gmail.com>
2024-01-09 10:29:10 +0100

    MINOR UPDATE : fixed check_double function

--- src/init_stack.c
+++ src/init_stack.c
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:25:40 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/08 16:12:17 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/09 10:28:38 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,23 @@ void	quit(long *to_free, int exit_code)
 int		check_double(long *to_check, int j)
 {
 	int	i;
-	int	k;
+	int k;
 
 	i = 0;
 	k = 1;
 	while (i < j)
 	{
 		if (to_check[i] == to_check[k])
+		{
+			printf("Double detected\n");	
 			return (1);
+		}
 		k++;
-		if (k == j)
+		if (k > j)
+		{
 			i++;
+			k = i + 1;
+		}
 	}
 	return (0);
 }
@@ -41,31 +47,26 @@ void    init_stack(int ac, char **av, t_node **a)
 {
 	long	cur_nb;
 	long	*pre_buff;
-	int		args;
+	char **av_copy;
 	int		i;
 	int 	j;
-
-	args = 0;
-	i = 1;
+	
+	// args = 0;
+	i = 0;
 	j = 0;
-	if (ac == 2)
-	{
-		// ! TO_DO : fait un decallage de 1 si chaine splitee
-		av = ft_split(av[1], ' ');
-		// av++;
-	}
 	// ! calculate number of args
-	while (av[i] != NULL)
-	{
+	if (ac == 2)
+		av_copy = ft_split(av[1], ' ');
+	else
+		av_copy = &av[1];
+	while (av_copy[i] != NULL)
 		i++;
-		args++;
-	}
-	pre_buff = malloc(sizeof(long) * args);
+	pre_buff = malloc(sizeof(long) * i);
 	if (!pre_buff)
 		exit(-1);
-	while (*++av != NULL)
+	while (*av_copy != NULL)
 	{
-		cur_nb = ft_atol(*av);
+		cur_nb = ft_atol(*av_copy);
 		printf("Current nb = %li\n", cur_nb);
 		if ((cur_nb < INT_MIN || cur_nb > INT_MAX))
 			quit(pre_buff, -1);
@@ -73,6 +74,7 @@ void    init_stack(int ac, char **av, t_node **a)
 		if (check_double(pre_buff, j))
 			quit(pre_buff, -1);
 		j++;
+		av_copy++;
 	}
 	 
 	
