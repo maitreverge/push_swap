ae4bed98dda0ed6b397450110ab71b8da618a0db
de_la_verge <verge.f@gmail.com>
2024-01-11 12:03:40 +0100

    🛠️ WORKING ON : rra/rrb/rrr

--- src/instructions_1.c
+++ src/instructions_1.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 12:18:21 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/11 11:03:23 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/11 11:59:58 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ void	sa(t_node **a, bool to_print)
 	int	temp;
 
 	if (lstsize(*a) < 2)
-	{
-		printf("stack a too small");
 		return ;
-	}
 	temp = (*a)->nb;
 	(*a)->nb = (*a)->next->nb;
 	(*a)->next->nb = temp;
@@ -33,10 +30,7 @@ void	sb(t_node **b, bool to_print)
 	int	temp;
 
 	if (lstsize(*b) < 2)
-	{
-		printf("stack b too small");
 		return ;
-	}
 	temp = (*b)->nb;
 	(*b)->nb = (*b)->next->nb;
 	(*b)->next->nb = temp;
--- src/instructions_3.c
+++ src/instructions_3.c
@@ -6,11 +6,35 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 12:18:27 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/25 12:22:51 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/11 12:03:03 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	rra(t_node **a, bool to_print)
+{
+	// The last element becomes the first one.
+	
+	if (to_print)
+		ft_printf("rra\n");
+}
+
+void	rrb(t_node **b, bool to_print)
+{
+	// The last element becomes the first one.
+	
+	if (to_print)
+		ft_printf("rrb\n");
+}
+
+void	rrr(t_node **a, t_node **b)
+{
+	rra(a, false);
+	rrb(b, false);
+	ft_printf("rrr\n");
+}
+
 
 /*
 
