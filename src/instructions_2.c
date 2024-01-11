c8d27edbb648346d560345ba17351a13a57641e4
de_la_verge <verge.f@gmail.com>
2024-01-11 11:22:52 +0100

    🛠️ WORKING ON : implementing ra/rb/rr

--- src/instructions_2.c
+++ src/instructions_2.c
@@ -6,12 +6,30 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 12:18:24 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/25 12:23:02 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/11 11:22:20 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+void	ra(t_node **a, bool to_print)
+{
+	if (to_print)
+		ft_print("ra\n")
+}
+
+void	rb(t_node **a, bool to_print)
+{
+	if (to_print)
+		ft_print("rb\n")
+}
+
+void	rr(t_node **a, t_node **b)
+{
+	ra(a, false);
+	rb(b, false);
+	ft_printf("rr\n");
+}
 /*
 
 ! ra (rotate a): 
