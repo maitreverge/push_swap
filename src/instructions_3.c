fe9bd60f8e2a3dbdeb53050ffa05e31421b0d02c
de_la_verge <verge.f@gmail.com>
2024-01-18 14:12:29 +0100

    fixed typo

--- src/instructions_3.c
+++ src/instructions_3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   instructions_3.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 12:18:27 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/19 12:35:29 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/20 07:01:54 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	rra(t_node **a, bool to_print)
 {
     if (to_print)
-        ft_printf("ra\n");
+        ft_printf("rra\n");
 }
 
 void	rrb(t_node **b, bool to_print)
 {
     if (to_print)
-        ft_printf("rb\n");
+        ft_printf("rrb\n");
 }
 
 void	rrr(t_node **a, t_node **b)
