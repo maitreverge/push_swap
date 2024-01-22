836ec55d0239d1ef42265d11e0b17c95c7207e5a
flverge_42 <verge.f@gmail.com>
2024-01-22 09:15:35 +0100

    fast save

--- src/linked_list_functions.c
+++ src/linked_list_functions.c
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 10:45:56 by flverge           #+#    #+#             */
-/*   Updated: 2024/01/22 08:22:44 by flverge          ###   ########.fr       */
+/*   Updated: 2024/01/22 08:43:59 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,28 @@ t_node	*lstnew(int number, int index)
 
 t_node	*lstlast(t_node *lst)
 {
+	t_node current;
 
+	current = lst;
+	if (!current)
+		return (0);
+	if (!current->next)	
+		return (current);
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	return (current);
 }
 
 
 void	lstadd_back(t_node **lst, t_node *new)
 {
+	t_node *tail;
 
+	tail = lstlast(*lst);
+	tail->next = new;
+	new->prev = tail;
 }
 
 void	lstadd_front(t_node **lst, t_node *new)
